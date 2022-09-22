c1f62e9d970229dfb025160c3cec583750b5a2aa
7Bit <87518152+7Bit420@users.noreply.github.com>
2022-09-22 14:01:17 +0000

    Update

--- test.cpp
+++ test.cpp
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <emscripten/emscripten.h>
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+#else
+#define EXTERN
+#endif
 
 int main() {
 
@@ -6,3 +13,13 @@ int main() {
 
     return 0;
 }
+
+
+EXTERN EMSCRIPTEN_KEEPALIVE char* log(char* str) {
+    printf("%s", str);
+    return str;
+}
+
+
+[Log] fd_write – 1 – 5245984 – 2 – 5245980 ([wasm code], line 0, x800)
+
