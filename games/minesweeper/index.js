418ab555a0033396cf2d9530e428e8ba818d13ac
7Bit <87518152+7Bit420@users.noreply.github.com>
2022-11-22 11:04:11 +1100

    Update

--- games/minesweeper/index.js
+++ games/minesweeper/index.js
@@ -1,8 +1,11 @@
 import tooManyMinesError from "./errors/tooManyMines";
 
+class MinesweeperManger extends EventTarget {
 
-class MinesweeperManger {
-
+    #mines = 0
+    #width = 0
+    #height = 0
+    #map = [[0]]
     #elms = {
         div: document.createElement('div'),
         modButtons: document.createElement('div'),
@@ -14,10 +17,58 @@ class MinesweeperManger {
         timer: document.createElement('h1'),
         levelMenu: document.createElement('select'),
     }
-    constructor(mines, width, height) {
+    constructor() {
+        super()
+
+        this.#map = []
+        this.#mines = 0
+        this.#width = 0
+        this.#height = 0
+    }
+
+    /**
+     * @param {number} mines 
+     * @param {number} width 
+     * @param {number} height 
+     */
+    init(mines, width, height) {
+        this.#map = new Array(width).fill(t => new Array(height).fill(0))
+
+        for (let i = 0; i < mines;) {
+            var [x, y] = [
+                Math.floor(Math.random() * width),
+                Math.floor(Math.random() * height),
+            ]
+            if (this.#map[x][y] >= 0) { this.#map[x][y] = -1; i++ }
+        }
+
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                var surroundingMines = this.#getSurroundingMines(x, y)
+                if (this.#map[x][y] == -1) {
+
+                } else {
+
+                }
+            }
+        }
+    }
+
+    imput(type, x, y) {
 
     }
 
+    #getSurroundingMines(x, y) {
+        return [
+            this.#map[x][y], this.#map[x][y], this.#map[x][y],
+            this.#map[x][y], this.#map[x][y], this.#map[x][y],
+            this.#map[x][y], this.#map[x][y], this.#map[x][y],
+        ].filter(t => t != -1)
+    }
 }
 
-class MinesweeperElm extends HTMLElement { }
+class MinesweeperElm extends HTMLElement {
+    init(grid) {
+
+    }
+}
